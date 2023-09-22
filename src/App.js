5b41737c60cb1ddcd2a72a124f5b64ce8a4c2542
Jerry John Thomas <68646979+JerryJohnThomas@users.noreply.github.com>
2023-09-23 01:49:29 +0530

    adjusted scroll only in pc and not in mobile

--- src/App.js
+++ src/App.js
@@ -3,22 +3,58 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ShapeDemo from "./Demo/ShapeDemo";
 import { Scrollbar } from "react-scrollbars-custom";
 import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+    const [windowDimensions, setWindowDimensions] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+
+    useEffect(() => {
+        // Function to update window dimensions
+        const updateWindowDimensions = () => {
+            setWindowDimensions({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+
+        // Add a window resize event listener
+        window.addEventListener("resize", updateWindowDimensions);
+
+        // Initial call to set dimensions
+        updateWindowDimensions();
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener("resize", updateWindowDimensions);
+        };
+    }, []); // Empty dependency array to run this effect only once
+
     return (
         <>
-            <Scrollbar
-                style={{ width: "100vw", height: "100vh" }}
-            >
-                <Router>
-                    <Routes>
-                        <Route path="/30pieces" element={<ShapeDemo />} />
-                        <Route path="/" element={<ShapeDemo />} />
-                    </Routes>
-                </Router>
-            </Scrollbar>
+            {windowDimensions.width > 600 && windowDimensions.height > 600 ? (
+                <Scrollbar style={{ width: "100vw", height: "100vh" }}>
+                    <Everything_App />
+                </Scrollbar>
+            ) : (
+                <Everything_App />
+            )}
         </>
     );
 }
+
+let Everything_App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/30pieces" element={<ShapeDemo />} />
+                <Route path="/" element={<ShapeDemo />} />
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
