c2c62305e020d8ac789e53167d233bd038bd83b9
Jerry John Thomas <68646979+JerryJohnThomas@users.noreply.github.com>
2023-09-30 00:40:23 +0530

    basic media query

--- src/Home/HomeLanding.js
+++ src/Home/HomeLanding.js
@@ -7,15 +7,15 @@ function HomeLanding() {
         <>
             <div className="container_fullscreen playFair_text  sd_container1 bg_color7">
                 <div className="sd_heading1 font_size3">30 Pieces Replication</div>
-                <div className="home_container flexDirection_row">
+                <div className="home_container flexDirection_rowB_cols">
                     <div className="roboto_text flex_center flexDirection_col  font_size_2_3">
-                        <div className="font_size_2_4">How To Make a Triangle</div>
+                        <div className="button_pre_text font_size_2_4">How To Make a Triangle</div>
                         <Button_custom text="here" url="/30pieces" />
                     </div>
 
                     <div className="roboto_text flex_center flexDirection_col font_size_2_3">
-                        <div className="font_size_2_4">Train with Images</div>
-                        <Button_custom text="here" url="/train"/>
+                        <div className="button_pre_text font_size_2_4">Train with Images</div>
+                        <Button_custom text="here" url="/train" />
                     </div>
                 </div>
             </div>
--- src/ImageComparator/CompareHome.js
+++ src/ImageComparator/CompareHome.js
@@ -7,7 +7,7 @@ function CompareHome() {
       <>
           <div className="container_fullscreen playFair_text  sd_container1 bg_color6">
               <div className="sd_heading0 font_size3">Training</div>
-              <div className="home_container flexDirection_row">
+              <div className="home_container flexDirection_rowB_cols">
                   <div className="roboto_text flex_center flexDirection_col  font_size_2_3">
                       <div className="font_size_2_4">How To Make a Triangle</div>
                   </div>
